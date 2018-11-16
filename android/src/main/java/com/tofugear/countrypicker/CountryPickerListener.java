86a38a7ce851d809995e2cf015b3966e93273159
Narendra Bagade <narendra.bagade@copart.com>
2018-11-16 15:28:37 -0600

    Update CountryPickerListener.java

--- android/src/main/java/com/tofugear/countrypicker/CountryPickerListener.java
+++ android/src/main/java/com/tofugear/countrypicker/CountryPickerListener.java
@@ -1,9 +1,12 @@
 package com.tofugear.countrypicker;
 
+import com.facebook.react.bridge.ReadableMap;
+
 /**
  * Inform the client which country has been selected
  *
  */
 public interface CountryPickerListener {
-	public void onSelectCountry(String name, String code, String phoneCode);
+	// public void onSelectCountry(String code,String name, ReadableMap response);
+	public void onSelectCountry(ReadableMap response);
 }
