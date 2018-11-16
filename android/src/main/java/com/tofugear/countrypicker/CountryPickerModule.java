3bb5510593c8bad96b6169c54b316a36b10d333d
Narendra Bagade <narendra.bagade@copart.com>
2018-11-16 15:00:09 -0600

    Update CountryPickerModule.java
    cutom update

--- android/src/main/java/com/tofugear/countrypicker/CountryPickerModule.java
+++ android/src/main/java/com/tofugear/countrypicker/CountryPickerModule.java
@@ -1,6 +1,7 @@
 package com.tofugear.countrypicker;
-
 import android.app.Activity;
+import android.app.FragmentManager;
+
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.NativeModule;
@@ -10,22 +11,24 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.bridge.ReadableNativeArray;
+import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.UiThreadUtil;
 
-import java.util.ArrayList;
-
 public class CountryPickerModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
+    private Activity mActivity=null;
     private CountryPicker mostRecentCountryPicker;
 
     // note that webView.isPaused() is not Xwalk compatible, so tracking it poor-man style
     private boolean isPaused;
 
-    public CountryPickerModule(ReactApplicationContext reactContext) {
+    public CountryPickerModule(ReactApplicationContext reactContext, Activity activity) {
         super(reactContext);
-        reactContext.addLifecycleEventListener(this);
+        mActivity=activity;
     }
 
     @Override
@@ -34,50 +37,36 @@ public class CountryPickerModule extends ReactContextBaseJavaModule implements L
     }
 
     @ReactMethod
-    public void show(final ReadableMap configs, final Callback callback) throws Exception {
+    public void show(final ReadableMap readableMap,final Callback callback) throws Exception {
         if (this.isPaused) {
             return;
         }
-        final Activity currentActivity = getCurrentActivity();
-        if (currentActivity == null) {
-            return;
-        }
 
+        ReadableArray array = readableMap.getArray("jsonData");
+        final CustomeData customeData = new CustomeData();
+        if(array !=null){
+            customeData.setJsInput(array);
+        }
         UiThreadUtil.runOnUiThread(new Runnable() {
-            public void run() {
-                ArrayList<String> countries = null;
-                if (configs.hasKey("countryList")) {
-                    final ReadableArray countryList = configs.getArray("countryList");
-                    countries = new ArrayList<>(countryList.size());
-                    for (int i = 0; i < countryList.size(); ++i) {
-                        // In the format of: code|phone|name
-                        String country = countryList.getString(i);
-                        if (country != null) {
-                          countries.add(country);
-                        }
+                public void run() {
+                    final CountryPicker picker = CountryPicker.newInstance(readableMap,customeData);
+                    picker.setListener(new CountryPickerListener() {
+                        @Override
+                        public void onSelectCountry(ReadableMap data) {
+                            WritableMap response= Arguments.createMap();
+                            WritableMap mapData = recursivelyDeconstructReadableMap(data);
+                            picker.dismiss();
+//                            response.putString("code",code);
+//                            response.putString("name",name);
+                            response.putMap("selectedObject",mapData);
+                            callback.invoke(response);
+                          }
+                    });
+                    mostRecentCountryPicker = picker;
+                    if(mActivity !=null) {
+                        picker.show(mActivity.getFragmentManager(), "COUNTRY_PICKER");
                     }
                 }
-                final CountryPicker picker = CountryPicker.newInstance(
-                        configs.getString("title"),
-                        configs.getString("searchHintText"),
-                        configs.getString("preferredCountryCodes"),
-                        countries);
-                picker.setListener(new CountryPickerListener() {
-                    @Override
-                    public void onSelectCountry(String name, String code, String phoneCode) {
-                        WritableMap response= Arguments.createMap();
-
-                        picker.dismiss();
-                        response.putString("name",name);
-                        response.putString("code",code);
-                        response.putString("phoneCode",phoneCode);
-
-                        callback.invoke(response);
-                    }
-                });
-                mostRecentCountryPicker = picker;
-                picker.show(currentActivity.getFragmentManager(), "COUNTRY_PICKER");
-            }
         });
     }
 
@@ -110,5 +99,69 @@ public class CountryPickerModule extends ReactContextBaseJavaModule implements L
     @Override
     public void onHostDestroy() {
         this.isPaused = true;
+    }
+
+
+    private WritableMap recursivelyDeconstructReadableMap(ReadableMap readableMap) {
+        ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
+        WritableMap deconstructedMap = Arguments.createMap();
+        while (iterator.hasNextKey()) {
+            String key = iterator.nextKey();
+            ReadableType type = readableMap.getType(key);
+            switch (type) {
+                case Null:
+                    deconstructedMap.putNull(key);
+                    break;
+                case Boolean:
+                    deconstructedMap.putBoolean(key, readableMap.getBoolean(key));
+                    break;
+                case Number:
+                    deconstructedMap.putDouble(key, readableMap.getDouble(key));
+                    break;
+                case String:
+                    deconstructedMap.putString(key, readableMap.getString(key));
+                    break;
+                case Map:
+                    deconstructedMap.putMap(key, recursivelyDeconstructReadableMap(readableMap.getMap(key)));
+                    break;
+                case Array:
+                    deconstructedMap.putArray(key, recursivelyDeconstructReadableArray(readableMap.getArray(key)));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Could not convert object with key: " + key + ".");
+            }
+
+        }
+        return deconstructedMap;
+    }
+
+    private WritableArray recursivelyDeconstructReadableArray(ReadableArray readableArray) {
+        WritableArray deconstructedList = Arguments.createArray();
+        for (int i = 0; i < readableArray.size(); i++) {
+            ReadableType indexType = readableArray.getType(i);
+            switch(indexType) {
+                case Null:
+                    deconstructedList.pushNull();
+                    break;
+                case Boolean:
+                    deconstructedList.pushBoolean(readableArray.getBoolean(i));
+                    break;
+                case Number:
+                    deconstructedList.pushDouble(readableArray.getDouble(i));
+                    break;
+                case String:
+                    deconstructedList.pushString(readableArray.getString(i));
+                    break;
+                case Map:
+                    deconstructedList.pushMap(recursivelyDeconstructReadableMap(readableArray.getMap(i)));
+                    break;
+                case Array:
+                    deconstructedList.pushArray(recursivelyDeconstructReadableArray(readableArray.getArray(i)));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Could not convert object at index " + i + ".");
+            }
+        }
+        return deconstructedList;
     }
 }
