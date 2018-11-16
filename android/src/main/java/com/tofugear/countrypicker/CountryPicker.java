83ad3dcf35abb63ed296b53d56d73ba620809507
Narendra Bagade <narendra.bagade@copart.com>
2018-11-16 14:58:46 -0600

    custom update

--- android/src/main/java/com/tofugear/countrypicker/CountryPicker.java
+++ android/src/main/java/com/tofugear/countrypicker/CountryPicker.java
@@ -11,7 +11,10 @@ import java.util.Locale;
 import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.DialogFragment;
 import android.text.Editable;
@@ -26,13 +29,17 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+
 public class CountryPicker extends DialogFragment implements
-		Comparator<Country> {
+		Comparator<ResponseData> {
 	/**
 	 * View components
 	 */
 	private EditText searchEditText;
 	private ListView countryListView;
+	static CustomeData customeData;
 
 	/**
 	 * Adapter for the listview
@@ -40,14 +47,15 @@ public class CountryPicker extends DialogFragment implements
 	private CountryListAdapter adapter;
 
 	/**
-	 * Hold all countries, sorted by country name
+	 * Hold all data, sorted by name
 	 */
-	private List<Country> allCountriesList;
+
+	private List<ResponseData> responseDataList;
 
 	/**
-	 * Hold countries that matched user query
+	 * Hold data that matched user query
 	 */
-	private List<Country> selectedCountriesList;
+	private List<ResponseData> selectedResponseData;
 
 	/**
 	 * Listener to which country user selected
@@ -63,41 +71,66 @@ public class CountryPicker extends DialogFragment implements
 		this.listener = listener;
 	}
 
+	public EditText getSearchEditText() {
+		return searchEditText;
+	}
+
+	public ListView getCountryListView() {
+		return countryListView;
+	}
+
 	/**
-	 * Set all countries with code and name from res/raw/countries.json
+	 * Convenient function to get currency code from country code currency code
+	 * is in English locale
 	 *
+	 * @param countryCode
 	 * @return
 	 */
-	private List<Country> setAllCountriesFromJSON() {
-		if (allCountriesList == null) {
+	public static Currency getCurrencyCode(String countryCode) {
+		try {
+			return Currency.getInstance(new Locale("en", countryCode));
+		} catch (Exception e) {
+
+		}
+		return null;
+	}
+
+	private List<ResponseData> getData(CustomeData customeData,String codeKey,String nameKey) {
+		if(customeData == null)
+		{
+			return null;
+		}
+		if (responseDataList == null) {
 			try {
-				allCountriesList = new ArrayList<Country>();
+				responseDataList = new ArrayList<ResponseData>();
 
-				// Read from local file
-				String allCountriesString = readFileAsString(getActivity());
-				JSONObject jsonObject = new JSONObject(allCountriesString);
-				Iterator<?> keys = jsonObject.keys();
+				ReadableArray array = customeData.getJsInput();
 
-				// Add the data to all countries list
-				while (keys.hasNext()) {
-					String key = (String) keys.next();
-					Country country = new Country();
-					country.setCode(key);
-					String[] parts=jsonObject.getString(key).split("\\|");
-					country.setPhoneCode(parts[0]);
-					country.setName(parts[1]);
-					allCountriesList.add(country);
+				for(int i=0 ;i < array.size();i++){
+					ReadableMap readableMap = array.getMap(i);
+					ResponseData responseData = new ResponseData();
+					if(codeKey!=null && !codeKey.equals("") && readableMap.hasKey(codeKey)){
+						responseData.setCode(readableMap.getString(codeKey));
+					}else{
+						responseData.setCode("");
+					}
+					if(nameKey !=null && !nameKey.equals("") && readableMap.hasKey(nameKey)){
+						responseData.setName(readableMap.getString(nameKey));
+					}else{
+						responseData.setName("");
+					}
+					responseData.setResponse(readableMap);
+					responseDataList.add(responseData);
 				}
-
 				// Sort the all countries list based on country name
-				Collections.sort(allCountriesList, this);
+				// Collections.sort(responseDataList,this);
 
 				// Initialize selected countries with all countries
-				selectedCountriesList = new ArrayList<Country>();
-				selectedCountriesList.addAll(allCountriesList);
+				selectedResponseData = new ArrayList<ResponseData>();
+				selectedResponseData.addAll(responseDataList);
 
 				// Return
-				return allCountriesList;
+				return responseDataList;
 
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -107,101 +140,27 @@ public class CountryPicker extends DialogFragment implements
 	}
 
 	/**
-	 * Set all countries from the country list given
-	 *
-	 * Per entry in the format of: code|phone|name
-	 */
-	private void setAllCountries(ArrayList<String> countries) {
-		if (allCountriesList != null)
-			return;
-		allCountriesList = new ArrayList<Country>();
-		for (String country_s: countries) {
-			String[] parts = country_s.split("\\|");
-			Country country = new Country();
-			country.setCode(parts[0]);
-			country.setPhoneCode(parts[1]);
-			country.setName(parts[2]);
-			allCountriesList.add(country);
-		}
-		// Sort the all countries list based on country name
-		Collections.sort(allCountriesList, this);
-		// Initialize selected countries with all countries
-		selectedCountriesList = new ArrayList<Country>();
-		selectedCountriesList.addAll(allCountriesList);
-	}
-
-	/**
-	 * move the countries in the preferred list to top
-	 *
-	 * @param preferredList
-	 * @return
-	 */
-	private void setPreferredCountries(String preferredList) {
-		if (preferredList != null && !preferredList.isEmpty()) {
-			String[] codes = preferredList.split(" ");
-
-			// get code list
-			ArrayList<String> codeList = new ArrayList<>();
-			Iterator<Country> keys = allCountriesList.iterator();
-			while (keys.hasNext()) {
-				Country country = (Country) keys.next();
-				codeList.add(country.getCode());
-			}
-
-			ArrayList<Country> preferredCountries = new ArrayList<>();
-			// find index of certain country
-			for (String code: codes) {
-				int index = codeList.indexOf(code);
-				Country country = allCountriesList.get(index);
-				preferredCountries.add(country);
-			}
-			// remove those countries and add to top
-			for (int i = preferredCountries.size()-1; i >= 0; i--) {
-				allCountriesList.remove(preferredCountries.get(i));
-				allCountriesList.add(0, preferredCountries.get(i));
-			}
-			// Initialize selected countries with all countries
-			selectedCountriesList = new ArrayList<Country>();
-			selectedCountriesList.addAll(allCountriesList);
-		}
-	}
-
-	/**
-	 * R.string.countries is a json string which is Base64 encoded to avoid
-	 * special characters in XML. It's Base64 decoded here to get original json.
-	 *
-	 * @param context
-	 * @return
-	 * @throws java.io.IOException
-	 */
-	private static String readFileAsString(Context context)
-			throws java.io.IOException {
-		String base64 = context.getResources().getString(R.string.countries);
-		byte[] data = Base64.decode(base64, Base64.DEFAULT);
-		return new String(data, "UTF-8");
-	}
-
-	/**
 	 * To support show as dialog
 	 *
-	 * @param dialogTitle The title
-	 * @param searchHint The hint placeholder to show
-	 * @param preferredCountryCodes List of preferred codes, delimited by space
-	 * @praam countries Optional list of countries to be used. Default list is used if not provided
-	 *
-	 * @return New instance of the CountryPicker
 	 */
-	public static CountryPicker newInstance(
-			String dialogTitle,
-			String searchHint,
-			String preferredCountryCodes,
-			ArrayList<String> countries) {
+	public static CountryPicker newInstance(ReadableMap readableMap,CustomeData customeData) {
+		CountryPicker.customeData = customeData;
 		CountryPicker picker = new CountryPicker();
 		Bundle bundle = new Bundle();
-		bundle.putString("dialogTitle", dialogTitle);
-		bundle.putString("searchHint", searchHint);
-		bundle.putString("preferredCountryCodes", preferredCountryCodes);
-		bundle.putStringArrayList("countries", countries);
+		bundle.putString("dialogTitle", readableMap.getString("title"));
+		bundle.putBoolean("isImageRequired", readableMap.getBoolean("isImageRequired"));
+		bundle.putBoolean("isSearchable", readableMap.getBoolean("isSearchable"));
+		//bundle.putSerializable("data",customeData);
+		if(!readableMap.hasKey("codeKey")  || (readableMap.getString("codeKey") == null || readableMap.getString("codeKey").equals(""))){
+			bundle.putString("codeKey", "");
+		}else if(readableMap.hasKey("codeKey")){
+			bundle.putString("codeKey", readableMap.getString("codeKey"));
+		}
+		if(!readableMap.hasKey("nameKey") || (readableMap.getString("nameKey") == null || readableMap.getString("nameKey").equals(""))){
+			bundle.putString("nameKey", "");
+		}else{
+			bundle.putString("nameKey", readableMap.getString("nameKey"));
+		}
 		picker.setArguments(bundle);
 		return picker;
 	}
@@ -214,55 +173,66 @@ public class CountryPicker extends DialogFragment implements
 			Bundle savedInstanceState) {
 		// Inflate view
 		View view = inflater.inflate(R.layout.country_picker, null);
-
+		// Set dialog title if show as dialog
 		Bundle args = getArguments();
-		if (args.getStringArrayList("countries") != null) {
-			setAllCountries(args.getStringArrayList("countries"));
-		} else {
-			setAllCountriesFromJSON();
+		boolean isSearchable=false;
+		boolean isImageRequired=false;
+		CustomeData customeData = CountryPicker.customeData;
+		String codeKey = "";
+		String nameKey = "";
+		if (args != null) {
+			isImageRequired = args.getBoolean("isImageRequired");
+			String dialogTitle = args.getString("dialogTitle");
+			isSearchable = args.getBoolean("isSearchable");
+			//customeData = (CustomeData) args.getSerializable("data");
+			if(args.getString("codeKey") !=null){
+				codeKey=args.getString("codeKey");
+			}
+			if(args.getString("nameKey") !=null){
+				nameKey=args.getString("nameKey");
+			}
+			getDialog().setTitle(dialogTitle);
+
+			int width = getResources().getDimensionPixelSize(
+					R.dimen.cp_dialog_width);
+			int height = getResources().getDimensionPixelSize(
+					R.dimen.cp_dialog_height);
+			getDialog().getWindow().setLayout(width, height);
 		}
-
-		String codes = args.getString("preferredCountryCodes");
-		setPreferredCountries(codes);
-
-		String dialogTitle = args.getString("dialogTitle");
-		getDialog().setTitle(dialogTitle);
-
-		String searchHint = args.getString("searchHint");
-
-		int width = getResources().getDimensionPixelSize(
-				R.dimen.cp_dialog_width);
-		int height = getResources().getDimensionPixelSize(
-				R.dimen.cp_dialog_height);
-		getDialog().getWindow().setLayout(width, height);
-
+		getData(customeData,codeKey,nameKey);
 		// Get view components
 		searchEditText = (EditText) view
 				.findViewById(R.id.country_picker_search);
-		// set hint text
-		searchEditText.setHint(searchHint);
+		if(!isSearchable){
+			searchEditText.setVisibility(View.GONE);
+		}
 		countryListView = (ListView) view
 				.findViewById(R.id.country_picker_listview);
 
 		// Set adapter
-		adapter = new CountryListAdapter(getActivity(), selectedCountriesList);
+		adapter = new CountryListAdapter(getActivity(), selectedResponseData,isImageRequired);
 		countryListView.setAdapter(adapter);
 
 		// Inform listener
 		countryListView.setOnItemClickListener(new OnItemClickListener() {
+
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 				if (listener != null) {
-					Country country = selectedCountriesList.get(position);
-					listener.onSelectCountry(country.getName(),
-							country.getCode(),country.getPhoneCode());
+					ResponseData data = selectedResponseData.get(position);
+//					listener.onSelectCountry(data.getCode(),data.getName(),
+//							data.getResponse());
+					listener.onSelectCountry(data.getResponse());
+
+
 				}
 			}
 		});
 
 		// Search for which countries matched user query
 		searchEditText.addTextChangedListener(new TextWatcher() {
+
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before,
 					int count) {
@@ -278,7 +248,7 @@ public class CountryPicker extends DialogFragment implements
 				search(s.toString());
 			}
 		});
-
+		setCancelable(false);
 		return view;
 	}
 
@@ -290,12 +260,12 @@ public class CountryPicker extends DialogFragment implements
 	 */
 	@SuppressLint("DefaultLocale")
 	private void search(String text) {
-		selectedCountriesList.clear();
+		selectedResponseData.clear();
 
-		for (Country country : allCountriesList) {
-			if (country.getName().toLowerCase(Locale.ENGLISH)
+		for (ResponseData data : responseDataList) {
+			if (data.getName().toLowerCase(Locale.ENGLISH)
 					.contains(text.toLowerCase())) {
-				selectedCountriesList.add(country);
+				selectedResponseData.add(data);
 			}
 		}
 
@@ -306,8 +276,18 @@ public class CountryPicker extends DialogFragment implements
 	 * Support sorting the countries list
 	 */
 	@Override
-	public int compare(Country lhs, Country rhs) {
+	public int compare(ResponseData lhs, ResponseData rhs) {
 		return lhs.getName().compareTo(rhs.getName());
 	}
 
+	@Override
+	public void show(FragmentManager manager, String tag) {
+		try {
+			FragmentTransaction ft = manager.beginTransaction();
+			ft.add(this, tag);
+			ft.commitAllowingStateLoss();
+		} catch (IllegalStateException e) {
+			Log.i("CameraModule", "Exception in picker dialog", e);
+		}
+	}
 }
